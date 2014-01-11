998feebb953949d2d2f818509e3ec7f1413bfeb5
Suz Hinton <suz.hinton@gmail.com>
2014-01-11 00:18:05 -0800

    getting some moves

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 var options = {
   // ** REQUIRED **
-  access_token:  'eQBwhd5FIoMs0_pqYzkaZ2P1u8OEOYNBdbvXv9Dv2oftOkvMKxWC7ejuTwbl0q7-kKMwPvEBJ55RAnYEZaPxlCzIBmUtBLpsaym2RYjpp5gDwoQTw2eSTw'
+  access_token:  'Je5CDuGC9ORcrdAxf3gA40QnKHlnMDyjGKNSPxdpEdkw4eX-yYDAVsY3UMdy4oJfrsusto4OhIz7HNtl_NYFMVECdgRlo_GULMgGZS0EumxrKbZFiOmnmAPChBPDZ5JP'
 }
 
 var up = require('jawbone-up')(options);
 
 var mee = up.me.get({}, function(dataaaa, resp) {
   console.log(dataaaa, resp);
+});
+
+var movez = up.moves.get({}, function(movedata, resp) {
+  console.log(movedata, resp);
 })
