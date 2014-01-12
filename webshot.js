05e7ce448d855c7852c4d880c470c92d23a93419
Suz Hinton <suz.hinton@gmail.com>
2014-01-12 00:57:51 -0800

    fixing webshot and html

--- webshot.js
+++ webshot.js
@@ -1,5 +1,5 @@
 var webshot = require('webshot');
 
-webshot('localhost:8000', 'tmp/today.png', function(err) {
+webshot('http://purr.noopkat.com/', 'tmp/today.png', function(err) {
   // screenshot now saved to google.png
 });
