fe65da2a4748e9c398afc826a557ce238c422edc
Suz Hinton <suz.hinton@gmail.com>
2014-02-13 01:03:06 -0800

    cleanup post hackathon

--- service.js
+++ service.js
@@ -13,53 +13,54 @@ http.createServer(function (req, res) {
 
   var view = {};
   var options = {
-  // jawbone up access token
-  access_token:  ''
-}
+    // jawbone up access token
+    access_token:  ''
+  };
 
-var up = require('jawbone-up')(options);
+  var up = require('jawbone-up')(options);
 
-var movez = up.moves.get({}, function(movedata, resp) {
-  //storing largest and smallest step numbers
-  var largest = 0;
-  var smallest = 1000000;
-  // storing the hour that largest step was
-  var lhour = 0;
-  // storing the hour that smallest step was
-  var shour = 0;
-  console.log(movedata, JSON.parse(resp).data.items[0]);
-  var move = JSON.parse(resp).data.items[0];
-  var hourlykeys = Object.keys(move.details.hourly_totals);
-  var movehourlen = hourlykeys.length;
-  console.log(movehourlen);
-  // find largest and smallest step hours
-  for (var key in move.details.hourly_totals) {
-    var steps = move.details.hourly_totals[key].steps;
+  var movez = up.moves.get({}, function(movedata, resp) {
+    // storing largest and smallest step number defaults
+    var largest = 0,
+        smallest = 1000000,
+        // storing the hour that largest and smallest step was
+        lhour = 0,
+        shour = 0;
 
-    // comparing largest
-    if (steps > largest) {
-      largest = steps;
-      lhour = key.substr(8, 2);
+    console.log(movedata, JSON.parse(resp).data.items[0]);
+
+    var move = JSON.parse(resp).data.items[0],
+        hourlykeys = Object.keys(move.details.hourly_totals),
+        movehourlen = hourlykeys.length;
+
+    // find largest and smallest step hours
+    // probably should do Math.max and Math.min instead (readability mostly) but the data is not in a flat array already
+    for (var key in move.details.hourly_totals) {
+      var steps = move.details.hourly_totals[key].steps;
+
+      // comparing largest
+      if (steps > largest) {
+        largest = steps;
+        lhour = key.substr(8, 2);
+      }
+
+      // comparing smallest
+      if (steps < smallest) {
+        smallest = steps;
+        shour = key.substr(8, 2);
+      }
+
     }
 
-    // comparing smallest
-    if (steps < smallest) {
-      smallest = steps;
-      shour = key.substr(8, 2);
-    }
+    // push to obj for template to use
+    view.lowsteps = smallest;
+    view.highsteps = largest;
+    view.lowhour = shour;
+    view.highhour = lhour;
 
-  }
-  // print largest and smallest
-  console.log(smallest, largest);
-  view.lowsteps = smallest;
-  view.highsteps = largest;
-  view.lowhour = shour;
-  view.highhour = lhour;
-
-  console.log(view);
-   var stream = mu.compileAndRender('index.html', view);
-  util.pump(stream, res);
-  
-});
+    var stream = mu.compileAndRender('index.html', view);
+    util.pump(stream, res);
+    
+  });
 
 }).listen(8000);
